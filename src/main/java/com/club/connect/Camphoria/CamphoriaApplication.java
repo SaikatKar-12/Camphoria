75898089e1e69ae4cffe18ded94f8810083643c4
Encryption-prince <subhammaity6827@gmail.com>
2025-03-29 11:49:24 +0530

    Initial commit

---
+++ src/main/java/com/club/connect/Camphoria/CamphoriaApplication.java
@@ -0,0 +1,13 @@
+package com.club.connect.Camphoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamphoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamphoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/club/connect/Camphoria/CamphoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.club.connect.Camphoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamphoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
